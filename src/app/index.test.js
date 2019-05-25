6f0b6ef0b9c760b166bd3394276c91a245bfa8f9
Nabil Anam <nabilcoding@gmail.com>
2019-05-25 19:52:45 +0600

    remove async duplication

--- src/app/index.test.js
+++ src/app/index.test.js
@@ -2,13 +2,16 @@ const request = require('supertest')
 const app = require('./index')
 
 describe('GET path /', () => {
-  test('should return 200 status', async () => {
-    const res = await request(app).get('/')
+  let res = null
+  beforeAll(async () => {
+    res = await request(app).get('/')
+  })
+
+  test('should return 200 status', () => {
     expect(res.statusCode).toBe(200)
   })
 
-  test('should return json body', async () => {
-    const res = await request(app).get('/')
+  test('should return json body', () => {
     expect(res.body).toEqual({ msg: 'welcome' })
   })
 })
