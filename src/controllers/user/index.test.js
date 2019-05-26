2201d8478b9c8efd1bf3ebcd338bd66da4a16144
Nabil Anam <nabilcoding@gmail.com>
2019-05-26 15:03:52 +0600

    use in memory db in previous tests

--- src/controllers/user/index.test.js
+++ src/controllers/user/index.test.js
@@ -1,41 +1,45 @@
-const User = require('../../models/User')
+const memoryDB = require('../../database/memory')
 const controller = require('./index')
+const config = require('config')
+
+beforeAll(() => memoryDB.start(), config.get('timeout'))
+afterAll(() => memoryDB.stop())
 
 describe('User controller -> create', () => {
-  const username = 'abcwtw'
-  const email = 'abtwc@xyz.com'
-  const password = 'lmnopqrs'
+  let person = {
+    username: 'abc',
+    email: 'abc@abc.com',
+    password: 'abc'
+  }
 
-  test('should return token when successful', () => {
-    jest
-      .spyOn(User.prototype, 'save')
-      .mockImplementation(() => Promise.resolve({ id: 123 }))
+  test('should return token (200) when no duplicate entry found', () =>
+    controller
+      .create(person.username, person.email, person.password)
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.data.split('.').length).toBe(3)
+      }))
 
-    return controller.create(username, email, password).then(response => {
-      expect(response.status).toBe(200)
-      expect(response.data.split('.').length).toBe(3)
-    })
-  })
+  test('should return (400) when username or email exists', () =>
+    controller
+      .create(person.username, person.email, person.password)
+      .catch(err => expect(err.status).toBe(400)))
 
-  test('should return 400 status when username or email exists', () => {
-    jest
-      .spyOn(User.prototype, 'save')
-      .mockImplementation(() =>
-        Promise.reject(new Error(User.status.DUPLICATE_KEY))
-      )
+  test('should return (500) for when username undefined', () =>
+    controller
+      .create(undefined, person.email, person.password)
+      .catch(err => expect(err.status).toBe(500)))
 
-    return controller
-      .create(username, email, password)
-      .catch(err => expect(err.status).toBe(400))
-  })
+  test('should return (500) for when email undefined', () =>
+    controller
+      .create(person.username, undefined, person.password)
+      .catch(err => expect(err.status).toBe(500)))
 
-  test('should return 500 status for unknown error', () => {
-    jest
-      .spyOn(User.prototype, 'save')
-      .mockImplementation(() => Promise.reject(new Error('unknown')))
+  test('should return (500) for when password undefined', () =>
+    controller
+      .create(person.username, person.email, undefined)
+      .catch(err => expect(err.status).toBe(500)))
 
-    return controller
-      .create(username, email, password)
-      .catch(err => expect(err.status).toBe(500))
-  })
+  test('should return (500) for when no data provided', () =>
+    controller.create().catch(err => expect(err.status).toBe(500)))
 })
--- src/routes/users/index.test.js
+++ src/routes/users/index.test.js
@@ -1,9 +1,14 @@
+const config = require('config')
 const request = require('supertest')
+
+const memoryDB = require('../../database/memory')
 const app = require('../../app')
-const User = require('../../models/User')
+
+beforeAll(() => memoryDB.start(), config.get('timeout'))
+afterAll(() => memoryDB.stop())
 
 describe('POST /api/users', () => {
-  test('should return 400 status when data is empty', () =>
+  test('should return (400) when data is empty', () =>
     request(app)
       .post('/api/users')
       .send({})
@@ -11,7 +16,7 @@ describe('POST /api/users', () => {
       .expect('Content-Type', /json/)
       .expect(400))
 
-  test('should return 400 status when username is not provided', () =>
+  test('should return (400) when username is not provided', () =>
     request(app)
       .post('/api/users')
       .send({})
@@ -22,7 +27,7 @@ describe('POST /api/users', () => {
         expect(res.body.param).toBe('username')
       }))
 
-  test('should return 400 status when email is not provided', () =>
+  test('should return (400) when email is not provided', () =>
     request(app)
       .post('/api/users')
       .send({ username: 'abc' })
@@ -33,7 +38,7 @@ describe('POST /api/users', () => {
         expect(res.body.param).toBe('email')
       }))
 
-  test('should return 400 status when password is not provided', () =>
+  test('should return (400) when password is not provided', () =>
     request(app)
       .post('/api/users')
       .send({ username: 'abc', email: 'abc@abc.com' })
@@ -44,16 +49,11 @@ describe('POST /api/users', () => {
         expect(res.body.param).toBe('password')
       }))
 
-  test('should return 200 status when all data is provided', () => {
-    jest
-      .spyOn(User.prototype, 'save')
-      .mockImplementation(() => Promise.resolve({ id: 123 }))
-
-    return request(app)
+  test('should return (200) when all data is provided', () =>
+    request(app)
       .post('/api/users')
       .send({ username: 'abc', email: 'abc@abc.com', password: 'g45aw65w' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200)
-  })
+      .expect(200))
 })
