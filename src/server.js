e1af796fb167c4250a954a3282a8191133bcb1fc
Nabil Anam <nabilcoding@gmail.com>
2019-06-14 17:05:28 +0600

    set request timeout

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const config = require('config')
 const db = require('./database')
 const app = require('./app')
+const port = config.get('port')
 
 db.connect()
 
-const PORT = config.get('port')
-app.listen(PORT, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server started on http://localhost:${PORT}`)
+  console.log(`Server started on http://localhost:${port}`)
 })
+
+server.setTimeout(config.get('request_timeout'))
