a8d320214521035fb0a155b1a7dcb20c48fe8be6
Nabil Anam <nabilcoding@gmail.com>
2019-06-21 01:09:38 +0600

    add custom validation error support to ValidatorResponse

--- src/response/ValidatorResponse/index.js
+++ src/response/ValidatorResponse/index.js
@@ -1,8 +1,18 @@
 const ErrorResponse = require('../ErrorResponse')
 
 class ValidatorResponse extends ErrorResponse {
-  constructor(status, validation_errors) {
-    const { msg, param } = validation_errors.array()[0]
+  constructor(status, validation_error) {
+    let msg, param
+
+    if (typeof validation_error.array === 'function') {
+      const error = validation_error.array()[0]
+      msg = error.msg
+      param = error.param
+    } else {
+      msg = validation_error.msg
+      param = validation_error.param
+    }
+
     super(status, msg)
     this.param = param
   }
--- src/response/ValidatorResponse/index.test.js
+++ src/response/ValidatorResponse/index.test.js
@@ -1,9 +1,9 @@
 const ValidatorResponse = require('./index')
 
 describe('ValidatorResponse', () => {
-  test('should initialize properly', () => {
+  test('should initialize properly when (validationResult)', () => {
     const status = 200
-    const errors = {
+    const validationResult = {
       array: () => [
         {
           msg: 'wrong password',
@@ -12,7 +12,23 @@ describe('ValidatorResponse', () => {
       ]
     }
 
-    const response = new ValidatorResponse(status, errors)
+    const response = new ValidatorResponse(status, validationResult)
+
+    expect(response).toEqual({
+      status,
+      error: 'wrong password',
+      param: 'password'
+    })
+  })
+
+  test('should initialize properly when (error object)', () => {
+    const status = 200
+    const error = {
+      msg: 'wrong password',
+      param: 'password'
+    }
+
+    const response = new ValidatorResponse(status, error)
 
     expect(response).toEqual({
       status,
