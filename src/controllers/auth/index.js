f782d13115bcea9c6541c7e70cf6f701b0cc9255
Nabil Anam <nabilcoding@gmail.com>
2019-06-21 16:38:45 +0600

    update is_authenticated instead of auth_key in auth controller

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -4,7 +4,10 @@ const response = require('../response')
 const login_token = auth_key =>
   new Promise((resolve, reject) => {
     if (auth_key)
-      return User.findOneAndUpdate({ auth_key }, { auth_key: '' }).then(user =>
+      return User.findOneAndUpdate(
+        { auth_key },
+        { is_authenticated: true }
+      ).then(user =>
         user
           ? resolve(response.login_token(user.id))
           : reject(response.internal_server_error())
